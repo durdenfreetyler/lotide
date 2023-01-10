6db5392ff00fe911c30cccddf1dc6f42ba174332
vagrant <abrahamrodgers@gmail.com>
2023-01-10 06:07:49 +0000

    learned how to use console.assert and build a function to test.

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅✅");
+  } else {
+    console.log("❌❌❌❌.");
+  }
+};
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+assertEqual(1, 1);
