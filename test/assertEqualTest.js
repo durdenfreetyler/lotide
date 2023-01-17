7d20e9a2316e0824c68153008d2122509f8c9bc9
Abe <abrahamrodgers@gmail.com>
2023-01-16 19:06:59 -0800

    Created index.js that includes all lotide functions.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require("../assertEqual");
+// test / assertEqualTest.js;
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+assertEqual(1, 1);
---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const head = require("../head");
+
+console.log(head([5, 6, 7]))
+head(["Hello", "Lighthouse", "Labs"])
+
